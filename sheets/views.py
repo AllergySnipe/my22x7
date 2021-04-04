fe581b3e1d06dc7ab250296c4926737c8ffb8aea
AllergySnipe <39958084+AllergySnipe@users.noreply.github.com>
2021-04-04 20:06:41 +0530

    dynamically import csv content to sql

--- sheets/views.py
+++ sheets/views.py
@@ -5,6 +5,16 @@ from rest_framework.response import Response
 from . serializer import *
 import csv, json
 from django.core import serializers
+import mysql.connector
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = str(BASE_DIR).replace("\\","/")
+mydb = mysql.connector.connect(
+		host="localhost",
+		user="root",
+		password="",
+		database = "dep"
+	)
 # Create your views here. 
 
 class SheetView(APIView): 
@@ -14,7 +24,7 @@ class SheetView(APIView):
     def get(self, request):
         #e=Sheet.objects.get(title='t3')
         #(make_json(e.sheet))[1]
-        mydetail = [ {"title": detail.title,"sheet": make_json(detail.sheet)}
+        mydetail = [ {"title": detail.title,"sheet": make_json(detail.title,detail.sheet,0)}
         for detail in Sheet.objects.all()] 
         return Response(mydetail) 
 
@@ -23,11 +33,12 @@ class SheetView(APIView):
         serializer = SheetSerializer(data=request.data) 
         if serializer.is_valid(raise_exception=True): 
             serializer.save()
-            return Response(make_json((serializer.data["sheet"])[1:]))
+            return Response(make_json(serializer.data["title"],(serializer.data["sheet"])[1:],1))
 
-def make_json(csvFilePath):
+def make_json(title,csvFilePath, flag):
 
     data={}
+    print(title)
     csvFilePath = r"{}".format(csvFilePath)
     with open(csvFilePath, encoding='utf-8-sig') as csvf:
         csvReader = csv.DictReader(csvf)
@@ -36,4 +47,14 @@ def make_json(csvFilePath):
             key = a
             a+=1
             data[key] = rows
+        if(flag==1):
+                print(csvFilePath)
+                importCsvToDB(title,csvFilePath)
         return data
+
+def importCsvToDB(tablename,path):
+    query="LOAD DATA INFILE '"+BASE_DIR+"/"+path+"' INTO TABLE "+tablename+" FIELDS TERMINATED BY ',' ENCLOSED BY '\"' IGNORE 1 ROWS;"
+    print(query)
+    mycursor = mydb.cursor()
+    mycursor.execute(query)
+    mydb.commit()
